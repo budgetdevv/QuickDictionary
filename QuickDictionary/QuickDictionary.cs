8c213e01a8f3bab628b1e1c5791a75e6c3b5f83c
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2025-04-29 18:09:25 +0800

    Implemented ShouldFillEntriesArrayWithNullKey logic

--- QuickDictionary/QuickDictionary.cs
+++ QuickDictionary/QuickDictionary.cs
@@ -13,6 +13,19 @@ namespace QuickDictionary
             public KeyT Key;
 
             public ValueF Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Entry(KeyT key)
+            {
+                Key = key;
+                Unsafe.SkipInit(out Value);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Entry(KeyT key, ValueF value): this(key)
+            {
+                Value = value;
+            }
         }
 
         private static readonly BuiltConfig<KeyT, ConfigT> BUILT_CONFIG = BuiltConfig<KeyT, ConfigT>.Build();
@@ -43,28 +56,36 @@ namespace QuickDictionary
             out Entry[] entries,
             out byte[] freeEntryMarkers)
         {
-            throw new NotImplementedException();
+            Debug.Assert(unchecked((int) capacity) > 0);
 
-            // Debug.Assert(unchecked((int) capacity) > 0);
-            //
-            // if (BUILT_CONFIG.IsPow2Sized)
-            // {
-            //     // Round to next power of 2
-            //     // capacity =
-            // }
-            //
-            // entries = AllocateInternal<Entry>(capacity);
-            //
-            // freeEntryMarkers = BUILT_CONFIG.ShouldAllocateFreeEntryMarkers ?
-            //     AllocateInternal<byte>(capacity) :
-            //     [];
-            //
-            // return;
-            //
-            // static T[] AllocateInternal<T>(uint capacity)
-            // {
-            //     return GC.AllocateUninitializedArray<T>(unchecked((int) capacity));
-            // }
+            if (BUILT_CONFIG.IsPow2Sized)
+            {
+                // TODO:
+                throw new NotImplementedException();
+
+                // Round to next power of 2
+                // capacity =
+            }
+
+            entries = AllocateInternal<Entry>(capacity);
+
+            freeEntryMarkers = BUILT_CONFIG.ShouldAllocateFreeEntryMarkers ?
+                AllocateInternal<byte>(capacity) :
+                [];
+
+            if (BUILT_CONFIG.ShouldFillEntriesArrayWithNullKey)
+            {
+                var nullEntry = new Entry(BUILT_CONFIG.NullKey);
+
+                entries.AsSpan().Fill(nullEntry);
+            }
+
+            return;
+
+            static T[] AllocateInternal<T>(uint capacity)
+            {
+                return GC.AllocateUninitializedArray<T>(unchecked((int) capacity));
+            }
         }
     }
 }
